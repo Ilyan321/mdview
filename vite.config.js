f2c8424d279a5d8e7a2b73f45874429911ab75d9
Ilyan321 <ilyaankhan342@gmail.com>
2026-09-25 20:28:32 +0500

    fix(seo): remove generic emojis from meta tags and create proper SVG favicon, also fix vite base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/mdview/' : '/',
+  base: '/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 700,
